40d91da559b4168874fb4f0a32b1faba2eb169ed
Suraj Vasudev More <suraj.more@quickheal.com>
2022-10-21 16:59:05 +0530

    Simple Employee Rest API

---
+++ rest-api/employee.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Employee struct {
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	DeptId  int    `json:"deptId"`
+	Phone   int64  `json:"phone"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
+}
+
+var employees = []Employee{
+	Employee{
+		Id:      "1",
+		Name:    "Suraj",
+		DeptId:  1,
+		Phone:   9527957483,
+		Email:   "suraj.more@github.com",
+		Address: "washim",
+	},
+	Employee{
+		Id:      "2",
+		Name:    "Sanket",
+		DeptId:  2,
+		Phone:   9899799697,
+		Email:   "sanket.kasar@github.com",
+		Address: "Sangamner",
+	},
+}
+
+func getEmoployees(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, employees)
+}
+
+func getEmoployee(c *gin.Context) {
+	id := c.Param("id")
+	employee := Employee{}
+	for _, emp := range employees {
+		if emp.Id == id {
+			employee = emp
+		}
+	}
+	c.IndentedJSON(http.StatusOK, employee)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/emp", getEmoployees)
+	router.GET("/emp/:id", getEmoployee)
+
+	router.Run("localhost:8081")
+}
